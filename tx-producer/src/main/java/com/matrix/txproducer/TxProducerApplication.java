395a04bdd91506f2c0270b04c26b68d1a254ef82
yihaosun <sunyh@smartebao.com>
2022-06-30 08:56:12 +0800

    Initial commit

---
+++ tx-producer/src/main/java/com/matrix/txproducer/TxProducerApplication.java
@@ -0,0 +1,13 @@
+package com.matrix.txproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TxProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TxProducerApplication.class, args);
+    }
+
+}
---
+++ tx-producer/src/test/java/com/matrix/txproducer/TxProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matrix.txproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TxProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
